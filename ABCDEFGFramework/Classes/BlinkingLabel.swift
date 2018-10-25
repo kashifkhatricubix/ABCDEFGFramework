16999f7a77159af32d5aac6918c4050e79d6dfdf
Kashif Khatri <kashif.khatri@cubixlabs.com>
2018-10-25 19:47:53 +0500

    add open keyword

--- ABCDEFGFramework/Classes/BlinkingLabel.swift
+++ ABCDEFGFramework/Classes/BlinkingLabel.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BlinkingLabel: UILabel {
+open class BlinkingLabel: UILabel {
 
     public func startBlinking() {
         let options : UIViewAnimationOptions = [.repeat, .autoreverse]
